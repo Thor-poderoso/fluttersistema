cdc5b90491c9aa321eee4bdc7bae1a881fbaed87
THIAGO SILVEIRA FERNANDES SILVA <thiago.sfsilva2@senacsp.edu.br>
2023-04-06 22:28:34 -0300

    arruma icons

--- sistema/lib/screens/main/main_screen.dart
+++ sistema/lib/screens/main/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainSreen extends StatelessWidget{
   const MainSreen({super.key});
@@ -10,14 +11,24 @@ class MainSreen extends StatelessWidget{
         child: Row(
           children: [
             Expanded(
-              child: Container(
-                color: Colors.white,
-                child: const Text(
-                "Hello World!",
-                style: TextStyle(color: Colors.black),
-                ),
+              child: Column(
+                children: [
+                  DrawerHeader(
+                    child: Image.asset("../../assets/images/logo.png"),
+                  ),
+                  ListTile(
+                  leading: SvgPicture.asset("../../rassets/icons/menu_dashbord.svg", 
+                  color: Colors.white54,
+                  height: 16,
+                  ),
+                  title: const Text(
+                    "Dashboard",
+                    style: TextStyle(color: Colors.white54),
+                    ),
+                  ),
+              ],
               ),
-              ),
+            ),
               Expanded(
                 flex: 5,
                 child: Container(
