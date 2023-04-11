1a5bc4065c490cfa3dbe1f9bfbcae13efd0318d7
THIAGO SILVEIRA FERNANDES SILVA <thiago.sfsilva2@senacsp.edu.br>
2023-04-10 21:55:03 -0300

    arruma dir

--- sistema/lib/screens/main/main_screen.dart
+++ sistema/lib/screens/main/main_screen.dart
@@ -1,43 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class MainSreen extends StatelessWidget{
+class MainSreen extends StatelessWidget {
   const MainSreen({super.key});
+
+  static const dir = "../../assets";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  DrawerHeader(
-                    child: Image.asset("../../assets/images/logo.png"),
-                  ),
-                  ListTile(
-                  leading: SvgPicture.asset("../../rassets/icons/menu_dashbord.svg", 
-                  color: Colors.white54,
-                  height: 16,
-                  ),
-                  title: const Text(
-                    "Dashboard",
-                    style: TextStyle(color: Colors.white54),
-                    ),
-                  ),
-              ],
+        body: SafeArea(
+            child: Row(
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+              DrawerHeader(
+                child: Image.asset("$dir/images/logo.png"),
               ),
-            ),
-              Expanded(
-                flex: 5,
-                child: Container(
-                color:  Colors.blue, 
+              DrawerListTile(
+                title: "Dashboard",
+                svgSrc: "$dir/icons/menu_dashbord.svg",
+                press: () {},
               ),
-            ),
-          ],
-        )
-        )
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 5,
+          child: Container(
+            color: Colors.blue,
+          ),
+        ),
+      ],
+    )));
+  }
+}
+
+class DrawerListTile extends StatelessWidget {
+  const DrawerListTile({
+    Key? key,
+    required this.title,
+    required this.svgSrc,
+    required this.press,
+  }) : super(key: key);
+
+  final String title, svgSrc;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: press,
+      horizontalTitleGap: 0.0,
+      leading: SvgPicture.asset(
+        svgSrc,
+        color: Colors.white54,
+        height: 16,
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(color: Colors.white54),
+      ),
     );
   }
 }
