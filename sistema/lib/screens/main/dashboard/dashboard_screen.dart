578586ec653bc6eeab070dccdbbff66bf1ce1b6d
THIAGO SILVEIRA FERNANDES SILVA <thiago.sfsilva2@senacsp.edu.br>
2023-04-19 22:07:49 -0300

    arruma pieChart

--- sistema/lib/screens/main/dashboard/dashboard_screen.dart
+++ sistema/lib/screens/main/dashboard/dashboard_screen.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:sistema/constants.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 import 'components/header.dart';
 
 class DashboardScreen extends StatelessWidget {
-  const DashboardScreen({
-    super.key,
-  });
+  const DashboardScreen({Key? key}) : super(key: key);
 
   static const dirIcon = "../../assets/icons";
   static const dir = "../../assets/images";
 
   @override
   Widget build(BuildContext context) {
+    List<PieChartSectionData> paiChartSelectionData = [
+      PieChartSectionData(
+        color: primaryColor,
+        value: 25,
+        showTitle: false,
+        radius: 25,
+      ),
+      PieChartSectionData(
+        color: Color(0xFF26E5FF),
+        value: 20,
+        showTitle: false,
+        radius: 25,
+      ),
+      PieChartSectionData(
+        color: Color(0xFFFFCF26),
+        value: 10,
+        showTitle: false,
+        radius: 25,
+      ),
+      PieChartSectionData(
+        color: Color(0xFFEE2727),
+        value: 15,
+        showTitle: false,
+        radius: 25,
+      ),
+      PieChartSectionData(
+        color: primaryColor.withOpacity(0.1),
+        value: 25,
+        showTitle: false,
+        radius: 25,
+      ),
+    ];
     return SafeArea(
       child: SingleChildScrollView(
         padding: const EdgeInsets.all(defaultPadding),
@@ -39,8 +70,8 @@ class DashboardScreen extends StatelessWidget {
                         color: secondaryColor,
                         borderRadius: BorderRadius.circular(10)),
                     child: Column(
-                      children: const [
-                        Text(
+                      children: [
+                        const Text(
                           "Storage Details",
                           style: TextStyle(
                             fontSize: 18,
@@ -49,7 +80,11 @@ class DashboardScreen extends StatelessWidget {
                         ),
                         SizedBox(
                           height: 200,
-                          child: ,
+                          child: PieChart(
+                            PieChartData(
+                              sections: paiChartSelectionData,
+                            ),
+                          ),
                         ),
                       ],
                     ),
